e0ecc4b5952c3eb65665606fb63519399df63d74
hernandoponzon <hernandonunez13@gmail.com>
2022-05-23 02:02:17 -0500

    first commit

---
+++ scripts/loginscript.js
@@ -0,0 +1,16 @@
+var emailElement = document.getElementById("inputEmail");
+var passElement = document.getElementById("inputPassword");
+
+function login(){
+    localStorage.setItem("email", emailElement.value);
+    localStorage.setItem("password", passElement.value);
+    document.location.href="../html/main.html";
+    //window.location.assign("/path");
+
+    //parent.location=pagina.html;/
+    //window.location.href=pagina.html;
+}
+
+function singin(){
+    document.location.href="../html/singin.html";
+}
